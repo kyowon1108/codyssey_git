33f7af1c667f35c81eba357afaf18f6854ee391b
kyowon1108 <kyowon1108@gmail.com>
2025-07-14 13:20:53 +0900

    first commit

--- sort_calculator.py
+++ sort_calculator.py
@@ -15,13 +15,13 @@ def main():
         numbers = []
 
         for token in tokens:
-            num = float(token)
+            num = float(token) # 입력받은 숫자 float로 처리
             numbers.append(num)
 
         bubble_sort(numbers) # numbers 버블 정렬
 
         sorted_str = ' '.join(str(float(num)) for num in numbers) # 띄어쓰기 join해서 정렬된 float 출력
-        print(f"Sorted: {sorted_str}")
+        print(f"Sorted: {sorted_str}") # 모든 숫자 float로 출력
 
     except ValueError:
         print("Invalid input.")
